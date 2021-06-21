198d95cb9bc3c649f0d10a73af5e090f692a1103
spaceforestchu <spaceforestchu@gmail.com>
2021-06-21 16:37:01 -0400

    first

---
+++ src/components/Signup/Signup.js
@@ -0,0 +1,187 @@
+import React, { Component } from "react";
+import "./Signup.css";
+
+export class Signup extends Component {
+  state = {
+    firstName: "",
+    lastName: "",
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    firstNameError: "",
+    lastNameError: "",
+    usernameError: "",
+    emailError: "",
+    passwordError: "",
+    confirmPasswordError: "",
+  };
+
+  handleOnChange = (event) => {
+    this.setState(
+      {
+        [event.target.name]: event.target.value,
+      },
+      () => {
+        // console.log("Inside setState Callback");
+        // console.log(this.state.firstName);
+        // console.log(event.target.name, ":", event.target.value);
+      }
+    );
+  };
+
+  handleOnSubmit = (event) => {
+    event.preventDefault();
+
+    console.log(this.state);
+  };
+
+  handleOnBlur = (event) => {
+    console.log(event.target.name);
+    console.log("Hanlde onBlur Triggered");
+
+    if (this.state[event.target.name].length === 0) {
+      this.setState({
+        [`${event.target.name}Error`]: `${event.target.placeholder} cannot be empty`,
+      });
+    }
+  };
+
+  render() {
+    const {
+      firstName,
+      lastName,
+      username,
+      email,
+      password,
+      confirmPassword,
+      firstNameError,
+      lastNameError,
+      usernameError,
+      emailError,
+      passwordError,
+      confirmPasswordError,
+    } = this.state;
+
+    return (
+      <div className="container">
+        <div className="form-text">Sign up</div>
+
+        <div className="form-div">
+          <form className="form" onSubmit={this.handleOnSubmit}>
+            <div className="form-group-inline">
+              <div className="inline-container">
+                <label htmlFor="firstName">First Name</label>
+                <input
+                  type="text"
+                  id="firstName"
+                  value={firstName}
+                  placeholder="First Name"
+                  name="firstName"
+                  onChange={this.handleOnChange}
+                  autoFocus
+                  onBlur={this.handleOnBlur}
+                />
+                <div className="errorMessage">
+                  {firstNameError && firstNameError}
+                </div>
+              </div>
+
+              <div className="inline-container">
+                <label htmlFor="lastName">Last Name</label>
+                <input
+                  type="text"
+                  id="lastName"
+                  value={lastName}
+                  placeholder="Last Name"
+                  name="lastName"
+                  onChange={this.handleOnChange}
+                  onBlur={this.handleOnBlur}
+                />
+                <div className="errorMessage">
+                  {lastNameError && lastNameError}
+                </div>
+              </div>
+            </div>
+
+            <div className="form-group-block">
+              <div className="block-container">
+                <label htmlFor="email">Email</label>
+                <input
+                  type="text"
+                  id="email"
+                  value={email}
+                  placeholder="Email"
+                  onChange={this.handleOnChange}
+                  name="email"
+                  onBlur={this.handleOnBlur}
+                />
+                <div className="errorMessage">{emailError && emailError}</div>
+              </div>
+            </div>
+
+            <div className="form-group-block">
+              <div className="block-container">
+                <label htmlFor="username">Username</label>
+                <input
+                  type="text"
+                  id="username"
+                  value={username}
+                  placeholder="Username"
+                  onChange={this.handleOnChange}
+                  name="username"
+                  onBlur={this.handleOnBlur}
+                />
+                <div className="errorMessage">
+                  {usernameError && usernameError}
+                </div>
+              </div>
+            </div>
+
+            <div className="form-group-block">
+              <div className="block-container">
+                <label htmlFor="password">Password</label>
+                <input
+                  type="text"
+                  id="password"
+                  value={password}
+                  placeholder="Password"
+                  onChange={this.handleOnChange}
+                  name="password"
+                  onBlur={this.handleOnBlur}
+                />
+                <div className="errorMessage">
+                  {passwordError && passwordError}
+                </div>
+              </div>
+            </div>
+
+            <div className="form-group-block">
+              <div className="block-container">
+                <label htmlFor="confirmPassword">Confirm Password</label>
+                <input
+                  type="text"
+                  id="confirmPassword"
+                  value={confirmPassword}
+                  placeholder="Confirm Password"
+                  onChange={this.handleOnChange}
+                  name="confirmPassword"
+                  onBlur={this.handleOnBlur}
+                />
+                <div className="errorMessage">
+                  {confirmPasswordError && confirmPasswordError}
+                </div>
+              </div>
+            </div>
+
+            <div className="button-container">
+              <button>Submit</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Signup;
