fb71572d2837df3e55edbae87f3cb226ae5e8fb1
spaceforestchu <spaceforestchu@gmail.com>
2021-06-22 16:00:49 -0400

    second

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { isAlpha, isEmail, isAlphanumeric, isStrongPassword } from "validator";
 import "./Signup.css";
 
 export class Signup extends Component {
@@ -15,6 +16,8 @@ export class Signup extends Component {
     emailError: "",
     passwordError: "",
     confirmPasswordError: "",
+    onConfirmPasswordFocus: false,
+    isButtonDisabled: true,
   };
 
   handleOnChange = (event) => {
@@ -23,11 +26,126 @@ export class Signup extends Component {
         [event.target.name]: event.target.value,
       },
       () => {
-        // console.log("Inside setState Callback");
-        // console.log(this.state.firstName);
-        // console.log(event.target.name, ":", event.target.value);
+        if (
+          event.target.name === "firstName" ||
+          event.target.name === "lastName"
+        ) {
+          this.handleFirstNameAndLastNameInput(event);
+        }
+
+        if (event.target.name === "email") {
+          this.handleEmailInput();
+        }
+
+        if (event.target.name === "username") {
+          this.handleUsernameInput();
+        }
+        if (event.target.name === "password") {
+          this.handlePasswordInput();
+        }
+
+        if (event.target.name === "confirmPassword") {
+          this.handleConfirmPasswordInput();
+        }
       }
     );
+  };
+
+  handleConfirmPasswordInput = () => {
+    if (this.state.password !== this.state.confirmPassword) {
+      this.setState({
+        confirmPasswordError: "Password does not match!",
+      });
+    } else {
+      this.setState({
+        confirmPasswordError: "",
+      });
+    }
+  };
+
+  handlePasswordInput = () => {
+    if (this.state.onConfirmPasswordFocus) {
+      if (this.state.password !== this.state.confirmPassword) {
+        this.setState({
+          confirmPasswordError: "Password does not match",
+        });
+      } else {
+        this.setState({
+          confirmPasswordError: "",
+        });
+      }
+    }
+
+    if (this.state.password.length === 0) {
+      this.setState({
+        passwordError: "Password cannot be empty",
+      });
+    } else {
+      if (isStrongPassword(this.state.password)) {
+        this.setState({
+          passwordError: "",
+        });
+      } else {
+        this.setState({
+          passwordError:
+            "Password must contains 1 uppercase, 1 lowercase, 1 special character, 1 number and minimul of 8 charactors long",
+        });
+      }
+    }
+  };
+
+  handleEmailInput = () => {
+    if (this.state.email.length === 0) {
+      this.setState({
+        emailError: "Email cannot be empty",
+      });
+    } else {
+      if (isEmail(this.state.email)) {
+        this.setState({
+          emailError: "",
+        });
+      } else {
+        this.setState({
+          emailError: "Please, enter a valid email!",
+        });
+      }
+    }
+  };
+
+  handleFirstNameAndLastNameInput = (event) => {
+    if (this.state[event.target.name].length > 0) {
+      if (isAlpha(this.state[event.target.name])) {
+        this.setState({
+          [`${event.target.name}Error`]: "",
+        });
+      } else {
+        this.setState({
+          [`${event.target.name}Error`]: `${event.target.placeholder} can only have alphabet`,
+        });
+      }
+    } else {
+      this.setState({
+        [`${event.target.name}Error`]: `${event.target.placeholder} cannot be empty`,
+      });
+    }
+  };
+
+  handleUsernameInput = () => {
+    if (this.state.username.length === 0) {
+      this.setState({
+        usernameError: "Username cannot be empty",
+      });
+    } else {
+      if (isAlphanumeric(this.state.username)) {
+        this.setState({
+          usernameError: "",
+        });
+      } else {
+        this.setState({
+          usernameError: "Username can only have alphabet and number",
+        });
+      }
+    }
   };
 
   handleOnSubmit = (event) => {
@@ -38,11 +156,19 @@ export class Signup extends Component {
 
   handleOnBlur = (event) => {
     console.log(event.target.name);
-    console.log("Hanlde onBlur Triggered");
+    console.log("handle onBlur Triggered");
 
     if (this.state[event.target.name].length === 0) {
       this.setState({
         [`${event.target.name}Error`]: `${event.target.placeholder} cannot be empty`,
+      });
+    }
+  };
+
+  handleConfirmPasswordOnFocus = () => {
+    if (!this.state.onConfirmPasswordFocus) {
+      this.setState({
+        onConfirmPasswordFocus: true,
       });
     }
   };
@@ -167,6 +293,7 @@ export class Signup extends Component {
                   onChange={this.handleOnChange}
                   name="confirmPassword"
                   onBlur={this.handleOnBlur}
+                  onFocus={this.handleConfirmPasswordOnFocus}
                 />
                 <div className="errorMessage">
                   {confirmPasswordError && confirmPasswordError}
@@ -175,7 +302,9 @@ export class Signup extends Component {
             </div>
 
             <div className="button-container">
-              <button>Submit</button>
+              <button type="submit" disabled={this.state.isButtonDisabled}>
+                Submit
+              </button>
             </div>
           </form>
         </div>
